2b3b846fda2acd306938b73e5d7685add41e20a4
Sonia Espinosa <sespinosa@college.harvard.edu>
2014-04-04 16:18:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emotomap::Application.config.secret_key_base = '28b195b6c669e4bc886a85ca2049fbd05840095abd5ac101ca2c0c6893f88bc3bfae49cb01fdd372d5a26b13087f7bd4d2b6e33261ed2cba76a5a7fdf466c84c'
